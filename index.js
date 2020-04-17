a0df53ee13d6ce5093f9c0bd0c3e2b979389e3d5
SergSmith64 <sergsmith2008@yandex.ru>
2020-04-18 00:41:20 +0400

    feat: add new branch

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 window.onload = function() {
 
     var todoList = [];
+    let a = todoList.length;
+    console.log("__длина массива__ = ", a);
 
     document.getElementById('add').onclick = function() {
         var d = document.getElementById('in').nodeValue;
+        console.log(d);
     }
 }
