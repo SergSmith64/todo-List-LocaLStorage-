1e9dffeaee505fb8ee42edadf4096580dcff4301
SergSmith64 <sergsmith2008@yandex.ru>
2020-04-19 22:04:16 +0400

    feat: the localStorage is connected

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ window.onload = function() {
     // __ чтобы хранить список дел создаем глобальный пустой массив
     var todoList = [];
 
+    // __ проверяю: есть ли что-то в localStorage
+    if (localStorage.getItem('todo') != undefined) {
+        // __ тогда достаем из него, преобразовав из строки __
+        // __ в массив с помощью JSON.parse __
+        todoList = JSON.parse(localStorage.getItem('todo'));
+        // __ и все что там есть - выводим на страницу в OUT __
+        out();
+    }
+
     let a = todoList.length;
     console.log("__длина массива__ = ", a);
 
@@ -33,6 +42,11 @@ window.onload = function() {
         // __ добавляю сюда функцию что идет ниже ___
         // __ СПИСОК ГОТОВ -- the list is ready and displayed on the page __
         out();
+
+        // __ добавляю LocalStorage, предварительно преобразовав массив __
+        // __ в строку с помощью функции JSON.stringify __
+        // __ при нажатии кнопки делаю:
+        localStorage.setItem( 'todo', JSON.stringify(todoList) );
     }
 
     // __ делаю вывод ДЕЛ, введенных через INPUT __
@@ -41,9 +55,20 @@ window.onload = function() {
         // __ сначала делаю пустую строку, потом цикл
         var out = '';
         for (var key in todoList) {
+            // __ добавляю чек-боксы ___
+            // __ сначала проверяем состояние поле поля ЧЕК: temp.check = false;
+            if(todoList[key].check == true) {
+                // __ тогда вывожу в OUT __
+                out += '<input type="checkbox" checked>';
+            }
+            else {
+                out += '<input type="checkbox">';
+            }
+
             // __ сначала вывожу сформировавшийся список дел __
             out += todoList[key].todo + '<br>';
-            // __ теперь записываю его Айди OUT на странице __
+
+            // __ теперь записываю его в Айди OUT на странице __
             document.getElementById('out').innerHTML = out;
         }
     }
